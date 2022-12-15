90ccad2ea5972f09cad8b9bfbd5a61f23475a188
avery.Chae <chaehj91@gmail.com>
2022-12-15 15:49:32 +0900

    GARMIN clone

---
+++ main.js
@@ -0,0 +1,11 @@
+new Swiper('main .promotion > .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 7000
+  },
+  slidesPerView: 1,
+  // spaceBetween: 0,
+  // centeredSlides: true,
+});
+
